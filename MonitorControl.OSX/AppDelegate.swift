09e612e8b056ae1ec83a7a328a70c6f8f3c2e3c8
Mathew Kurian <MathewKurian@utexas.edu>
2016-09-26 22:50:16 -0700

    Initial Commit

---
+++ MonitorControl.OSX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MonitorControl.OSX
+//
+//  Created by Mathew Kurian on 9/26/16.
+//  Copyright © 2016 Mathew Kurian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
