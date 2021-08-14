ccdda83d33ea75625af0f12674d52d89a2284d64
waydabber <37590873+waydabber@users.noreply.github.com>
2021-08-14 17:01:25 +0200

    Missing ForceSwCellView

---
+++ MonitorControl/UI/Cells/ForceSwCellView.swift
@@ -0,0 +1,26 @@
+import Cocoa
+import os.log
+
+class ForceSwCellView: NSTableCellView {
+  @IBOutlet var button: NSButton!
+  var display: Display?
+  let prefs = UserDefaults.standard
+
+  override func draw(_ dirtyRect: NSRect) {
+    super.draw(dirtyRect)
+  }
+
+  @IBAction func buttonToggled(_ sender: NSButton) {
+    if let display = display {
+      switch sender.state {
+      case .off:
+        display.forceSw = true
+      case .on:
+        display.forceSw = false
+      default:
+        break
+      }
+      app.updateMenus()
+    }
+  }
+}
