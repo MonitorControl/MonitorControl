60257edadf14012427ad505778e418359b912b13
JoniVR <joni.VR@hotmail.com>
2021-10-01 20:11:18 +0200

    feat: close preference pane using cmd+w

--- MonitorControl/Extensions/Preferences+Extension.swift
+++ MonitorControl/Extensions/Preferences+Extension.swift
@@ -1,5 +1,7 @@
 //  Copyright © MonitorControl. @JoniVR, @theOneyouseek, @waydabber and others
 
+import Cocoa
+
 import Preferences
 
 extension Preferences.PaneIdentifier {
@@ -8,4 +10,14 @@ extension Preferences.PaneIdentifier {
   static let keyboard = Self("Keyboard")
   static let displays = Self("Displays")
   static let about = Self("About")
+}
+
+public extension PreferencesWindowController {
+  override func keyDown(with event: NSEvent) {
+    if event.modifierFlags.intersection(.deviceIndependentFlagsMask) == .command, let key = event.charactersIgnoringModifiers {
+      if key == "w" {
+        self.close()
+      }
+    }
+  }
 }
